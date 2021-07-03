83cb8d754ef05de6b37276eabc12342c80a0c15d
Airton Isaac Pereira <airton.isaac@gmail.com>
2021-07-03 19:14:09 -0300

    Configura acoes do django

--- blog/principal/models.py
+++ blog/principal/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 class Post(models.Model):  
     title = models.CharField(max_length=255)
@@ -8,3 +9,9 @@ class Post(models.Model):
     body = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("principal:detail", kwargs={"slug": self.slug})
